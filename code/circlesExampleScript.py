80105f4ca511ee7956efc516d9ea32741ee4487d
Tom Needham <needham.71@osu.edu>
2019-05-23 22:37:49 -0400

    edits

--- code/circlesExampleScript.py
+++ code/circlesExampleScript.py
@@ -5,6 +5,6 @@ Generate the 'concentric circles' example for clustering experiments
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_circles
 
-X1, y1 = make_circles(n_samples=500, noise = 0.02, random_state = 3)
+X1, y1 = make_circles(n_samples=50, noise = 0.02, random_state = 3)
 plt.scatter(X1[:,0],X1[:,1],c=y1)
 plt.show()
